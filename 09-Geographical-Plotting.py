22669d36245afbe524caec1b54764b8343d4bf11
Daniel Gallo <daniel.gallo@xodusgroup.com>
2025-08-07 11:54:08 +0800

    09 Choropleth USA

--- 09-Geographical-Plotting.py
+++ 09-Geographical-Plotting.py
@@ -1,6 +1,6 @@
-
 import plotly.graph_objects as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import pandas as pd
 
 init_notebook_mode(connected=True) 
 
@@ -15,7 +15,34 @@ data = dict(type ='choropleth',
 layout = dict (geo = {'scope': 'usa'})
 choromap = go.Figure(data =[data], layout = layout)
 
-choromap.write_html("USA choromap.html", auto_open=True)
+choromap.write_html("USA choropleth map.html", auto_open=True)
 
 
 iplot(choromap)
+
+# ----
+
+df = pd.read_csv("C:\\Users\\Daniel.Gallo\\OneDrive - Xodus Group\\Documents\\Data Science\\Py-DS-ML-Bootcamp-master\\Refactored_Py_DS_ML_Bootcamp-master\\09-Geographical-Plotting\\2011_US_AGRI_Exports.csv")
+df.head()
+
+data = dict (
+    type  ='Choropleth',
+    colorscale = 'YIOrRd',
+    locations = df['code'],
+    z = df['total exports'],
+    locationmode = 'USA-states',
+    text = df['text'],
+    marker = dict(line = dict(color = 'rgb(255,255,255)',width = 2)),
+    colorbar = {'title':"Millions USD"}
+    )
+
+layout = dict( 
+    title = '2011 USA Agriculture Exports by State',
+    geo = dict( scope = 'usa',
+               showlakes = True,
+               lakecolor = 'rgb(85,173,240)'
+    )
+)
+
+choromap2 = go.Figure( data = [data], layout = layout)
+choromap2.write_html("USA Agricultiral Exports Map.html", auto_open=True)
