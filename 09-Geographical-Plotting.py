ee4a157924c6277362dc5174ce365037b400b84c
Daniel Gallo <daniel.gallo@xodusgroup.com>
2025-08-07 16:04:19 +0800

    09 Choropleth maps - excercises

--- 09-Geographical-Plotting.py
+++ 09-Geographical-Plotting.py
@@ -15,19 +15,19 @@ data = dict(type ='choropleth',
 layout = dict (geo = {'scope': 'usa'})
 choromap = go.Figure(data =[data], layout = layout)
 
-choromap.write_html("USA choropleth map.html", auto_open=True)
+choromap.write_html("USA Choropleth Map.html", auto_open=True)
 
 
 iplot(choromap)
 
-# ----
+# ---- USA
 
 df = pd.read_csv("C:\\Users\\Daniel.Gallo\\OneDrive - Xodus Group\\Documents\\Data Science\\Py-DS-ML-Bootcamp-master\\Refactored_Py_DS_ML_Bootcamp-master\\09-Geographical-Plotting\\2011_US_AGRI_Exports.csv")
 df.head()
 
 data = dict (
-    type  ='Choropleth',
-    colorscale = 'YIOrRd',
+    type  = 'choropleth',
+    colorscale = 'ylorrd',
     locations = df['code'],
     z = df['total exports'],
     locationmode = 'USA-states',
@@ -45,4 +45,30 @@ layout = dict(
 )
 
 choromap2 = go.Figure( data = [data], layout = layout)
-choromap2.write_html("USA Agricultiral Exports Map.html", auto_open=True)
+iplot(choromap2)  ## creates interactive plot - pop up a new chrome window
+choromap2.write_html("USA Agricultural Exports Map.html")
+
+# ---- International
+
+df = pd.read_csv("C:\\Users\\Daniel.Gallo\\OneDrive - Xodus Group\\Documents\\Data Science\\Py-DS-ML-Bootcamp-master\\Refactored_Py_DS_ML_Bootcamp-master\\09-Geographical-Plotting\\2014_World_GDP.csv")
+df.head()
+
+data =  dict(
+    type = 'choropleth',
+    locations = df['CODE'],
+    z = df['GDP (BILLIONS)'],
+    text = df['COUNTRY'],
+    colorbar = {'title': 'GDP Billions US'}
+)
+
+layout = dict (
+    title = '2014 Global GDP',
+    geo = dict(
+        showframe = False,
+        projection = {'type':'mercator'}
+    )
+)
+
+choromap3 = go.Figure( data = [data], layout = layout)
+iplot(choromap3)
+choromap3.write_html("International GDP Map.html")
