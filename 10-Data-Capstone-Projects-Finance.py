e42550930ce8091991ffbe65f21d945286dbabde
Daniel Gallo <daniel.gallo@xodusgroup.com>
2026-01-11 10:37:06 +0800

    Capstone Project Finance

--- 10-Data-Capstone-Projects-Finance.py
+++ 10-Data-Capstone-Projects-Finance.py
@@ -6,17 +6,8 @@ import numpy as np
 import datetime
 import pickle
 
-df=pd.read_pickle("C:\\Users\\Daniel.Gallo\\OneDrive - Xodus Group\\Documents\\Data Science\\Py-DS-ML-Bootcamp-master\\Refactored_Py_DS_ML_Bootcamp-master\\10-Data-Capstone-Projects\\all_banks.pkl")
+bank_stocks=pd.read_pickle("C:\\Users\\Daniel.Gallo\\OneDrive - Xodus Group\\Documents\\Data Science\\Py-DS-ML-Bootcamp-master\\Refactored_Py_DS_ML_Bootcamp-master\\10-Data-Capstone-Projects\\all_banks.pkl")
 
-session = requests.Session()
-session.verify = certifi.where() 
+bank_stocks.xs(key= "Close", axis = 1, level = "Stock Info").max()
 
-
-start = datetime.datetime(2006,1,1)
-end =  datetime.datetime(2016,1,1)
-
-def get_stock_data(ticker, start, end, **kwargs):
-    return yf.download(ticker, start=start, end=end, progress=False, auto_adjust=False, threads=True, **kwargs)
-
-C = get_stock_data("C", start, end)
-
+a=5
