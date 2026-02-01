0f39d87c09fd3a87b92bd554c039575b9ab64619
Daniel Gallo <daniel.gallo.r@gmail.com>
2026-02-01 15:58:25 +0800

    Decision Tree & Random Forest Lesson & Project

--- 15-Decision-Trees-and-Random-Forests.py
+++ 15-Decision-Trees-and-Random-Forests.py
@@ -18,7 +18,7 @@ from sklearn.model_selection import train_test_split
 X = df.drop('Kyphosis', axis = 1)
 y = df['Kyphosis']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 # Decision Tree
 
@@ -30,3 +30,18 @@ dtree.fit(X_train, y_train)
 
 # Prediction and Evaluation
 
+predict = dtree.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+print(classification_report(y_test, predict))
+print(confusion_matrix(y_test, predict))
+
+from sklearn.ensemble import RandomForestClassifier
+
+rfc = RandomForestClassifier(n_estimators= 200)
+rfc.fit(X_train, y_train)
+rfc_pred = rfc.predict(X_test)
+
+print(classification_report(y_test, rfc_pred))
+print(confusion_matrix(y_test, rfc_pred))
