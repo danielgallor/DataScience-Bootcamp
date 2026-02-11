0725634b7f170d89bdad501b7d319cc1703b83ee
Daniel Gallo <daniel.gallo.r@gmail.com>
2026-02-11 15:32:42 +0800

    PCA Lesson

---
+++ 18-Principal-Component-Analysis.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+
+cancer.keys()
+
+print(cancer['DESCR'])
+
+df = pd.DataFrame(cancer['data'], columns = cancer['feature_names'])
+df.head()
+
+## PCA Visualisation
+    ## Fit the high dimesional data to find which are the principal components
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+scaler.fit(df)
+
+scaled_data = scaler.transform(df)
+
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components= 2)
+
+pca.fit(scaled_data)
+
+x_pca = pca.transform(scaled_data)
+
+scaled_data.shape
+
+x_pca
+
+# Now Plot
+
+plt.figure(figsize=(8,6))
+plt.scatter(x_pca[:,0], x_pca[:,1], c=cancer['target'], cmap='plasma')
+plt.xlabel('First Principal Component')
+plt.ylabel('Second Principal Component')
+plt.show()
+
+# Interpreting Components
+    # The components correspond to combinations of the original features, not directly to the features
+pca.components_
+
+df_comp = pd.DataFrame(pca.components_, columns= cancer['feature_names'])
+
+plt.figure(figsize=(12,6))
+sns.heatmap(df_comp, cmap='plasma')
+plt.show()
